32b0603733f082b956ffe4b2f63d44f6e3aca5d2
izelacerda <izelacerdajl@gmail.com>
2021-07-28 17:56:38 -0300

    Profile

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -2,7 +2,6 @@ import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
 import { RFValue } from "react-native-responsive-fontsize";
 import { CarDTO } from '../../dtos/CarDTO';
-import { RectButton } from "react-native-gesture-handler";
 
 export const Container = styled.View`
   flex: 1;
